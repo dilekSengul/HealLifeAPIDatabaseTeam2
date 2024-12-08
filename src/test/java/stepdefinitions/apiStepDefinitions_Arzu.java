7b0eddff7f7d86f1aa6bb5d86cc2aedb1d2629df
arzuv100 <arzuv@gmail.com>
2024-12-08 15:32:34 +0500

    arzu

--- src/test/java/stepdefinitions/apiStepDefinitions_Arzu.java
+++ src/test/java/stepdefinitions/apiStepDefinitions_Arzu.java
@@ -2,6 +2,7 @@ package stepdefinitions;
 
 import base.BaseTest;
 import io.cucumber.java.en.Given;
+import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import utilities.api.API_Methods;
@@ -12,11 +13,14 @@ import static utilities.api.API_Methods.fullPath;
 
 public class apiStepDefinitions_Arzu extends BaseTest {
 
+    String exceptionMessage = null;
+
     @Given("Api user sets {string} path parameters.")
     public void api_user_sets_path_parameters(String pathParameter) {
         API_Methods.pathParam(pathParameter);
 
     }
+
     @Given("Api user sends a GET request so saves the returned response.")
     public void api_user_sends_a_get_request_so_saves_the_returned_response() {
         response = given()
@@ -27,6 +31,7 @@ public class apiStepDefinitions_Arzu extends BaseTest {
         response.prettyPrint();
 
     }
+
     @Given("Api user verifies Status Code {int}.")
     public void api_user_verifies_status_code(Integer code) {
         response.then()
@@ -34,6 +39,7 @@ public class apiStepDefinitions_Arzu extends BaseTest {
                 .statusCode(code);
 
     }
+
     @Given("Api user verifies {string} information in the response body is {string}.")
     public void api_user_verifies_information_in_the_response_body_is(String key, String value) {
         response.then()
@@ -41,6 +47,7 @@ public class apiStepDefinitions_Arzu extends BaseTest {
                 .body(key, Matchers.equalTo(value));
 
     }
+
     @Given("Api user verifies the information in response body for entry with specified {int} index, including {string}, {string}, {string}, {string}, {string}, {string}.")
     public void api_user_verifies_the_information_in_response_body_for_entry_with_specified_index_including(int dataIndex, String source, String purpose, String name, String email, String note, String created_at) {
         repJP = response.jsonPath();
@@ -53,6 +60,51 @@ public class apiStepDefinitions_Arzu extends BaseTest {
         Assert.assertEquals(created_at, repJP.getString("lists[" + dataIndex + "].created_at"));
     }
 
+    @Given("The api user sends a GET request, then saves the returned response, and verifies that the status code is '403' with the reason phrase Forbidden.")
+    public void the_api_user_sends_a_get_request_then_saves_the_returned_response_and_verifies_that_the_status_code_is_with_the_reason_phrase_forbidden() {
+        response = given()
+                .spec(spec)
+                .when()
+                .get(fullPath);
+
+        response.prettyPrint();
+
+        response.then()
+                .assertThat()
+                .statusCode(403)
+                .body(Matchers.containsString("You do not have authorization or token error."));
+    }
+
+
+    @Given("Api user prepares a GET request containing {int} info to send to api visitorsId endpoint.")
+    public void api_user_prepares_a_get_request_containing_info_to_send_to_api_visitors_id_endpoint(int id) {
+        requestBody.put("id", id);
+
+        System.out.println("Body " + requestBody);
+    }
+
+    @Given("Api user sends a GET body and saves response.")
+    public void api_user_sends_a_get_body_and_saves_response() {
+        response = given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .when()
+                .body(requestBody.toString())
+                .get(fullPath);
+
+        response.prettyPrint();
+    }
+
+    @Given("Api user verifies the data in response body includes {string}, {string}, {string}, {string}, {string}.")
+    public void api_user_verifies_the_data_in_response_body_includes_and(String id, String source, String purpose, String name, String email) {
+        response.then()
+                .assertThat()
+                .body("lists.id", Matchers.equalTo(id),
+                        "lists.source", Matchers.equalTo(source),
+                        "lists.purpose", Matchers.equalTo(purpose),
+                        "lists.name", Matchers.equalTo(name),
+                        "lists.email", Matchers.equalTo(email));
+    }
+
 
 }
-
--- src/test/resources/features/api/visitors/A_US_009_VisitorsList.feature
+++ src/test/resources/features/api/visitors/A_US_009_VisitorsList.feature
@@ -11,5 +11,16 @@ Feature: As an administrator (admin) I should be able to access the Visitor List
     * Api user verifies the information in response body for entry with specified <dataIndex> index, including "<source>", "<purpose>", "<name>", "<email>", "<note>", "<created_at>".
 
     Examples:
-    | dataIndex | source   | purpose                                | name      | email                    | note                             | created_at          |
-    | 3         | Online   | Hi, wrote about the price for reseller | JohnWathe | somasesokiyo31@gmail.com | Hi, kam dashur të di çmimin tuaj | 2024-12-02 19:35:12 |
+    | dataIndex | source   | purpose                                    | name      | email                    | note                             | created_at          |
+    | 3         | Online   | Hi,   wrote about   the price for reseller | JohnWathe | somasesokiyo31@gmail.com | Hi, kam dashur të di çmimin tuaj | 2024-12-02 19:35:12 |
+
+@US
+  Scenario: Invalid Token when a GET request is sent to the /api/visitorsList endpoint with invalid authorization information,
+  it should be verified that the status code returned is 403 and
+  the message in the response body is "You do not have authorization or token error.".
+
+    * Api user sets "api/visitorsList" path parameters.
+    * The api user sends a GET request, then saves the returned response, and verifies that the status code is '403' with the reason phrase Forbidden.
+
+
+
