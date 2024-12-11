58904f74fd4dc6e00dcad1c0ada5b658d518bd04
Kevss <kevsermarulcu@gmail.com>
2024-12-11 20:40:04 +0300

    ManageEklemeYapıldı

--- src/test/java/Manage/Manage.java
+++ src/test/java/Manage/Manage.java
@@ -11,6 +11,14 @@ public class Manage {
             "Values (?,?,?,?,SHA2(?,256),?,?,?,?)" ;
     String US25="Select gender,email From  u201212290_heallifeqa.patients Where patient_name LIKE '%Jain%';";
 
+    private String US_30 = "SELECT name FROM visitors_book WHERE related_to = 'Maria Fernandis (4) (OPDN24)' AND purpose = 'Visit';";
+
+    private String US_31 = "SELECT cons_doctor, COUNT(*) AS instruction FROM consultant_register GROUP BYcons_doctor;";
+
+    private String US_32 = "SELECT * FROM consultant_register WHERE date >= '2024-11-18 00:00:00' AND date < '2024-12-01 00:00:00';";
+
+
+
 
     //***************Getter***************\\
 
@@ -26,4 +34,10 @@ public class Manage {
     public String getUS25() {
         return US25;
     }
+
+    public String getUS_30() {return US_30;}
+
+    public String getUS_31() {return US_31;}
+
+    public String getUS_32() {return US_32;}
 }
